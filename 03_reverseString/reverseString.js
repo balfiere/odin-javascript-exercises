a3d38db313863c5a4e5b0fbc46caef2c93b0b57c
balfiere <balfiere@proton.me>
2024-07-18 15:18:41 -0500

    finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let returnString = "";
 
+    for (let i = string.length; i > 0; i--) {
+        returnString += string.charAt(i-1);
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
