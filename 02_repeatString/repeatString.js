a7fd92cc5f822044bec6cb120287ad6bf449ddcd
balfiere <balfiere@proton.me>
2024-07-18 14:55:00 -0500

    finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    let returnString = "";
 
+    if (times < 0) {
+        return "ERROR";
+    } else if (times === 0) {
+        return "";
+    } else {
+        for (let i = 0; i < times; i++) {
+            returnString += string;
+        }
+        return returnString;
+    }
 };
 
 // Do not edit below this line
