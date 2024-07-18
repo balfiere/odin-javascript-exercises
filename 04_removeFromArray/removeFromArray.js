80f887f9fb07172efd686595b565aa6d531fa0b3
balfiere <balfiere@proton.me>
2024-07-18 16:33:12 -0500

    finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    toRemove.forEach((index) => {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === index) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    });
+
+    return array;
 };
 
 // Do not edit below this line
